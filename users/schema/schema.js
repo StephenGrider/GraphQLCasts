6cccd7a7d0cd1a8b40186eff2969dd2d83cde591
stephengrider <ste.grider@gmail.com>
2017-01-19 16:20:58 -0800

    added root query

---
+++ users/schema/schema.js
@@ -0,0 +1,28 @@
+const graphql = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+
+      }
+    }
+  }
+});
--- users/server.js
+++ users/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
 
 app.listen(4000, () => {
   console.log('Listening');
