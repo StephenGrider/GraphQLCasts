7beed5535f2a8eb52ba96b630fae95ff9600dd0d
stephengrider <ste.grider@gmail.com>
2017-01-19 16:28:09 -0800

    finished schema

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,9 +1,16 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema
 } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 }
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -21,8 +28,12 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-
+        return _.find(users, { id: args.id });
       }
     }
   }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
