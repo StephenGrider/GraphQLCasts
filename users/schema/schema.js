882036596ffa8fcfe19bd51f8ec36ad4e88fe617
stephengrider <ste.grider@gmail.com>
2017-01-25 16:12:01 -0800

    associations

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -7,12 +7,24 @@ const {
   GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType
+    }
   }
 });
 
