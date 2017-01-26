fd480777fd710f7d65f720c9521052fe34affe26
stephengrider <ste.grider@gmail.com>
2017-01-25 16:52:50 -0800

    finished user association

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -23,7 +23,11 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
     company: {
-      type: CompanyType
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data);
+      }
     }
   }
 });
