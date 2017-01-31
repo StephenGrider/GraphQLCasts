899a5a388a3fa069d5f9625805e2f4b42e013b2c
stephengrider <ste.grider@gmail.com>
2017-01-31 11:22:02 -0800

    query issued

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
   render() {
+    console.log(this.props);
+
     return (
       <div>
         SongList
@@ -19,4 +22,4 @@ const query = gql`
   }
 `;
 
-export default SongList;
+export default graphql(query)(SongList);
