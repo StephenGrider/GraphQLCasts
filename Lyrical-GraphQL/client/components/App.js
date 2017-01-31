58738fadb5710c042952699fbfd4de2767f645e0
stephengrider <ste.grider@gmail.com>
2017-01-31 12:06:33 -0800

    added react router

---
+++ Lyrical-GraphQL/client/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default ({ children }) => {
+  return <div className="container">{children}</div>;
+};
--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import App from './components/App';
 import SongList from './components/SongList';
 
 const client = new ApolloClient({});
@@ -10,7 +12,11 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
