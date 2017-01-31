c8c03b4c9c95e23b24417d9e130f08f6b87102a6
stephengrider <ste.grider@gmail.com>
2017-01-31 10:40:45 -0800

    added react

---
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SongList extends Component {
+  render() {
+    return (
+      <div>
+        SongList
+      </div>
+    );
+  }
+}
+
+export default SongList;
--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import SongList from './components/SongList';
+
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <SongList />
     </ApolloProvider>
   );
 };
