cf99ae42d77379be010386305da70f5ebfca8418
stephengrider <ste.grider@gmail.com>
2017-01-31 11:39:53 -0800

    added key

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -6,7 +6,7 @@ class SongList extends Component {
   renderSongs() {
     return this.props.data.songs.map(song => {
       return (
-        <li>
+        <li key={song.id} className="collection-item">
           {song.title}
         </li>
       );
@@ -17,9 +17,9 @@ class SongList extends Component {
     if (this.props.data.loading) { return <div>Loading...</div>; }
 
     return (
-      <div>
+      <ul className="collection">
         {this.renderSongs()}
-      </div>
+      </ul>
     );
   }
 }
@@ -27,6 +27,7 @@ class SongList extends Component {
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
