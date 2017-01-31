64c0b64fa4bf454c0d2b8ef6e409e3becee61753
stephengrider <ste.grider@gmail.com>
2017-01-31 11:35:12 -0800

    issue query

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -3,12 +3,22 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li>
+          {song.title}
+        </li>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props);
+    if (this.props.data.loading) { return <div>Loading...</div>; }
 
     return (
       <div>
-        SongList
+        {this.renderSongs()}
       </div>
     );
   }
