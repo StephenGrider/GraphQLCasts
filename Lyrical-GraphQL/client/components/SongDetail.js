1c583599e865cc51db7d768808d6b96a1f2d1e03
stephengrider <ste.grider@gmail.com>
2017-02-01 10:46:56 -0800

    finished query

--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
+    console.log(this.props);
     return (
       <div>
         <h3>Song Detail</h3>
@@ -10,4 +13,6 @@ class SongDetail extends Component {
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
