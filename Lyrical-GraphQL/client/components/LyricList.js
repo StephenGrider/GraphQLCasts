7e8ff03c0fa64f557af5e10408146f76945fde88
stephengrider <ste.grider@gmail.com>
2017-02-01 14:20:20 -0800

    added lyriclist

---
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LyricList extends Component {
+  render() {
+    return (
+      <ul>
+        LyricList
+      </ul>
+    );
+  }
+}
+
+export default LyricList;
--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -3,6 +3,7 @@ import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
   render() {
@@ -14,6 +15,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList />
         <LyricCreate songId={this.props.params.id} />
       </div>
     );
