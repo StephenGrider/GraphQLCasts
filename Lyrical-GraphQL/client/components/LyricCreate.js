4f588b379217eb39e186ee39ab255d8bcb3ca31c
stephengrider <ste.grider@gmail.com>
2017-02-01 14:07:15 -0800

    submit mutation

--- Lyrical-GraphQL/client/components/LyricCreate.js
+++ Lyrical-GraphQL/client/components/LyricCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricCreate extends Component {
   constructor(props) {
@@ -10,7 +12,12 @@ class LyricCreate extends Component {
   onSubmit(event) {
     event.preventDefault();
 
-
+    this.props.mutate({
+      variables: {
+        content: this.state.content,
+        songId: this.props.songId
+      }
+    }).then(() => this.setState({ content: '' }));
   }
 
   render() {
@@ -37,4 +44,4 @@ const mutation = gql`
   }
 `;
 
-export default LyricCreate;
+export default graphql(mutation)(LyricCreate);
--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -14,7 +14,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
-        <LyricCreate />
+        <LyricCreate songId={this.props.params.id} />
       </div>
     );
   }
