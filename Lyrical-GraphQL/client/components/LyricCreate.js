9cff7ffe5cefaa173edaf91ba1e18683b93fe82c
stephengrider <ste.grider@gmail.com>
2017-02-01 13:58:08 -0800

    added mutation

--- Lyrical-GraphQL/client/components/LyricCreate.js
+++ Lyrical-GraphQL/client/components/LyricCreate.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 
 class LyricCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { content: '' };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit.bind(this)}>
         <label>Add a Lyric</label>
-        <input />
+        <input
+          value={this.state.content}
+          onChange={event => this.setState({ content: event.target.value })}
+        />
       </form>
     );
   }
 }
+
+const mutation = gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        content
+      }
+    }
+  }
+`;
 
 export default LyricCreate;
