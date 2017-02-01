3847ff6d20eb993f9e815cc68778e14af039d055
stephengrider <ste.grider@gmail.com>
2017-01-31 18:15:35 -0800

    fix mutation

--- Lyrical-GraphQL/server/schema/mutations.js
+++ Lyrical-GraphQL/server/schema/mutations.js
@@ -39,7 +39,7 @@ const mutation = new GraphQLObjectType({
       type: SongType,
       args: { id: { type: GraphQLID } },
       resolve(parentValue, { id }) {
-        return Song.findOneAndRemove(id);
+        return Song.remove({ _id: id });
       }
     }
   }
