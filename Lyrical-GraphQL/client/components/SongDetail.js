070219981dc6441f5704d7f5e562e56557afa9e2
stephengrider <ste.grider@gmail.com>
2017-02-01 11:06:01 -0800

    watch for song data

--- Lyrical-GraphQL/client/components/SongDetail.js
+++ Lyrical-GraphQL/client/components/SongDetail.js
@@ -4,10 +4,13 @@ import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
-    console.log(this.props);
+    const { song } = this.props.data;
+
+    if (!song) { return <div>Loading...</div>; }
+
     return (
       <div>
-        <h3>Song Detail</h3>
+        <h3>{song.title}</h3>
       </div>
     );
   }
