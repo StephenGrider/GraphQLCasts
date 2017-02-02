5908c50b53d28884c93b952f97456c5ba7fb688c
stephengrider <ste.grider@gmail.com>
2017-02-02 15:19:04 -0800

    fixed bug

--- Lyrical-GraphQL/client/components/LyricCreate.js
+++ Lyrical-GraphQL/client/components/LyricCreate.js
@@ -40,6 +40,7 @@ const mutation = gql`
       lyrics {
         id
         content
+        likes
       }
     }
   }
--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -3,8 +3,18 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 class LyricList extends Component {
-  onLike(id) {
-    this.props.mutate({ variables: { id } });
+  onLike(id, likes) {
+    this.props.mutate({
+      variables: { id },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id,
+          __typename: 'LyricType',
+          likes: likes + 1
+        }
+      }
+    });
   }
 
   renderLyrics() {
@@ -15,7 +25,7 @@ class LyricList extends Component {
           <div className="vote-box">
             <i
               className="material-icons"
-              onClick={() => this.onLike(id)}
+              onClick={() => this.onLike(id, likes)}
             >
               thumb_up
             </i>
