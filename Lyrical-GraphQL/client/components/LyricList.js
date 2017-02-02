84ead6f6b3f8cd2ae8e168b048faa06a28f6cdf1
stephengrider <ste.grider@gmail.com>
2017-02-02 11:45:39 -0800

    show thumbs up icon

--- Lyrical-GraphQL/client/components/LyricList.js
+++ Lyrical-GraphQL/client/components/LyricList.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 class LyricList extends Component {
+  onLike(id) {
+    console.log(id);
+  }
+
   renderLyrics() {
     return this.props.lyrics.map(({ id, content }) => {
       return (
         <li key={id} className="collection-item">
           {content}
+          <i
+            className="material-icons"
+            onClick={() => this.onLike(id)}
+          >
+            thumb_up
+          </i>
         </li>
       );
     });
