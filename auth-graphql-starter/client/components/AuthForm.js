24927cf7317720353124fa704a27c5f933223820
stephengrider <ste.grider@gmail.com>
2017-02-08 14:53:55 -0800

    added error display

--- auth-graphql-starter/client/components/AuthForm.js
+++ auth-graphql-starter/client/components/AuthForm.js
@@ -32,6 +32,9 @@ class AuthForm extends Component {
               onChange={e => this.setState({ password: e.target.value})}
             />
           </div>
+          <div className="errors">
+            {this.props.errors.map(error => <div key={error}>{error}</div>)}
+          </div>
           <button className="btn">Submit</button>
         </form>
       </div>
--- auth-graphql-starter/client/components/LoginForm.js
+++ auth-graphql-starter/client/components/LoginForm.js
@@ -5,12 +5,19 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { errors: [] };
+  }
+
   onSubmit({ email, password }) {
     this.props.mutate({
       variables: { email, password },
       refetchQueries: [{ query }]
     }).catch(res => {
       const errors = res.graphQLErrors.map(error => error.message);
+      this.setState({ errors });
     });
   }
 
@@ -18,7 +25,10 @@ class LoginForm extends Component {
     return (
       <div>
         <h3>Login</h3>
-        <AuthForm onSubmit={this.onSubmit.bind(this)} />
+        <AuthForm
+          errors={this.state.errors}
+          onSubmit={this.onSubmit.bind(this)}
+        />
       </div>
     );
   }
