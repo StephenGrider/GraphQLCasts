f00aa681cdcd9b0c19933c5d900998965255d732
stephengrider <ste.grider@gmail.com>
2017-02-08 12:10:01 -0800

    Added rr

--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id
@@ -10,9 +13,10 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>
-        Auth Starter
-      </div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
