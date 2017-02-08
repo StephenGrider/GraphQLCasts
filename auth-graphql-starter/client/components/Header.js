d643bf8b37af03aad2814514b314051bea2677ff
stephengrider <ste.grider@gmail.com>
2017-02-08 12:22:12 -0800

    added query to fetch current user

--- auth-graphql-starter/client/components/Header.js
+++ auth-graphql-starter/client/components/Header.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 class Header extends Component {
   render() {
+    console.log(this.props.data);
+
     return (
       <div>
         Header
@@ -10,4 +14,4 @@ class Header extends Component {
   }
 }
 
-export default Header;
+export default graphql(query)(Header);
---
+++ auth-graphql-starter/client/queries/CurrentUser.js
@@ -0,0 +1,10 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  {
+    user {
+  		id
+      email
+    }
+  }
+`;
