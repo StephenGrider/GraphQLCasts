f18b38d47ab1b2a7687e92acd83691994fdfaaa7
stephengrider <ste.grider@gmail.com>
2017-02-08 12:44:07 -0800

    header shows buttons

--- auth-graphql-starter/client/components/Header.js
+++ auth-graphql-starter/client/components/Header.js
@@ -3,13 +3,27 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
-  render() {
-    console.log(this.props.data);
+  renderButtons() {
+    const { loading, user } = this.props.data;
 
+    if (loading) { return <div />; }
+
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return (
+        <div>You're not signed in.</div>
+      );
+    }
+  }
+
+  render() {
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          {this.renderButtons()}
+        </div>
+      </nav>
     );
   }
 }
