45827cf838a11467c930e2cd4657085a57b4677f
stephengrider <ste.grider@gmail.com>
2017-02-08 10:42:23 -0800

    tested mutation

--- auth-graphql-starter/server/schema/schema.js
+++ auth-graphql-starter/server/schema/schema.js
@@ -2,7 +2,9 @@ const graphql = require('graphql');
 const { GraphQLSchema } = graphql;
 
 const RootQueryType = require('./types/root_query_type');
+const mutation = require('./mutations');
 
 module.exports = new GraphQLSchema({
-  query: RootQueryType
+  query: RootQueryType,
+  mutation
 });
--- auth-graphql-starter/server/schema/types/root_query_type.js
+++ auth-graphql-starter/server/schema/types/root_query_type.js
@@ -1,8 +1,11 @@
 const graphql = require('graphql');
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLID } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType'
+  name: 'RootQueryType',
+  fields: {
+    dummyField: { type: GraphQLID }
+  }
 });
 
 module.exports = RootQueryType;
