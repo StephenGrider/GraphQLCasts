348c6fb202e95bd5d8572e2adb7be975d014c64b
stephengrider <ste.grider@gmail.com>
2017-02-08 11:03:43 -0800

    added login mutation

--- auth-graphql-starter/server/schema/mutations.js
+++ auth-graphql-starter/server/schema/mutations.js
@@ -18,6 +18,24 @@ const mutation = new GraphQLObjectType({
       resolve(parentValue, { email, password }, req) {
         return AuthService.signup({ email, password, req });
       }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout();
+        return user;
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.login({ email, password, req });
+      }
     }
   }
 });
