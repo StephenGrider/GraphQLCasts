3206223bbf950334b583a966c324f729cac4cacd
stephengrider <ste.grider@gmail.com>
2017-02-17 08:49:48 -0800

    form

--- auth-graphql-starter/client/components/SignupForm.js
+++ auth-graphql-starter/client/components/SignupForm.js
@@ -3,12 +3,19 @@ import AuthForm from './AuthForm';
 import { graphql } from 'react-apollo';
 import mutation from '../mutations/Signup';
 import query from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
 
 class SignupForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (nextProps.data.user && !this.props.data.user) {
+      hashHistory.push('/dashboard');
+    }
   }
 
   onSubmit({ email, password }) {
@@ -34,4 +41,6 @@ class SignupForm extends Component {
   }
 }
 
-export default graphql(mutation)(SignupForm);
+export default graphql(query)(
+  graphql(mutation)(SignupForm)
+);
